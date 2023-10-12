a78aeb10d2839a4df64ea898df839053c2f511f6
KwikKill <gabriel@blaisot.org>
2023-10-12 11:10:53 +0200

    Add profile picture to user dashboard

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -12,20 +12,27 @@ fetch_user_inscription_full(user.value.id)
 	<div class="flex ml-1">
 		<div id="profile" class="w-1/4 items-center">
 			<h1 class="text-center m-3 font-bold text-4xl">Mon compte </h1>
-			<div class="m-4 rounded-2xl">
-				<div class="m-2">
-					<p class="text-xl">Pseudo : <em>{{user.username}}</em>  <fa-awesome-icon size="2xs" icon="fa-solid fa-pencil" /></p>
-					<p class="text-xl">Email : <em>{{user.email}}</em>  <fa-awesome-icon size="2xs" icon="fa-solid fa-pencil" /></p>
-					<p class="text-xl">Mot de passe : <em>***********</em>  <fa-awesome-icon size="2xs" icon="fa-solid fa-pencil" /></p>
-				</div>
-				<div class="m-1 flex justify-around text-md bg-blue-700 rounded p-1 w-16">
-					<svg v-if="role=='dev'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-					<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-					</svg>
-					{{role}}
+			<div class="myr-2 rounded-2xl">
+				<div class="my-2 flex flex-row justify-items-center place-items-center">
+					<div class="m-2 flex justify-items-center place-items-center">
+						<img class="m-2 rounded-full max-w-full max-h-full w-16" :src="user.image"/>
+					</div>
+					<div>
+						<p class="text-xl">Pseudo : <em>{{user.username}}</em><fa-awesome-icon class="ml-2" size="2xs" icon="fa-solid fa-pencil" /></p>
+						<p class="text-xl">Email : <em>{{user.email}}</em><fa-awesome-icon class="ml-2" size="2xs" icon="fa-solid fa-pencil" /></p>
+						<p class="text-xl">Mot de passe : <em>**********</em><fa-awesome-icon class="ml-2" size="2xs" icon="fa-solid fa-pencil" /></p>
+						<div class="flex justify-items-center place-items-center"><p class="text-xl">Role : </p>
+							<div class="m-1 flex justify-around text-md bg-blue-700 rounded p-1 w-16">
+								<svg v-if="role=='dev'" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+									<path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+								</svg>
+								{{role}}
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="m-4">	
+			<div class="myr-2 ml-1">	
 				<button @click="delete_account()" class="center rounded transition duration-150 ease-in-out p-2 font-bold text-white bg-red-600 hover:ring hover:ring-pink-500" style="display: flex; align-items: center;">Supprimer son compte</button>
 			</div>
 		</div>
