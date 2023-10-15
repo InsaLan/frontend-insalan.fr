ddcbc51b1642182e687f9c0235f9c24f9251f94e
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-10-15 23:07:25 +0200

    simplify tournament card style

--- src/components/TournamentCard.vue
+++ src/components/TournamentCard.vue
@@ -15,16 +15,12 @@ const registerUrl = "tournament/"+tour.id+"/register"
 const detailUrl = "tournament/"+tour.id
 </script>
 <template>
-  <div class="max-w-1/3 min-w-1/3 container h-min bg-cyan-900 shadow-lg">
+  <div class="grid bg-cyan-900 shadow-lg place-items-center gap-2">
     <img :src="tour.logo" class="overflow-clip w-screen aspect-video"/>
-    <div>
-      <p class="pt-1 text-center">
-        {{ tournament.teams.length }}/{{ maxTeam }} Equipes | Cashprize: {{ cashprize }}
-      </p>
-    </div>
-    <div class="flex place-content-center pb-3">
-      <a :href="detailUrl" class="p-2 mr-7 rounded border-green-600 border-2">Plus d'infos</a>
-      <a :href="registerUrl" class="p-2 ml-7 bg-green-600 center rounded">S'inscrire</a> <!-- TODO: turns button into component-->
+    <p>{{tour.teams.length}}/{{maxTeam}} Equipes | Cashprize: {{cashprize}}</p>
+    <div class="flex w-2/3 justify-between mb-3">
+      <a :href="detailUrl" class="p-2 rounded border-green-600 border-2">Plus d'infos</a>
+      <a :href="registerUrl" class="p-2 bg-green-600 center rounded">S'inscrire</a> <!-- TODO: turns button into component-->
     </div>
   </div>
 </template>
