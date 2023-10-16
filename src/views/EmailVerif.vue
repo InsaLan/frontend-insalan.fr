2d3f49308388f4e7d54a84405eed4f863c8fe21f
SirZalen <enzosicard2003@gmail.com>
2023-10-16 11:40:04 +0200

    First email validation page look

--- src/views/EmailVerif.vue
+++ src/views/EmailVerif.vue
@@ -1,1 +1,17 @@
-<template></template>
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <div>
+            <p class=" text-center text-5xl md:m-8"> Création de compte </p>
+            <p class=" text-center text-2xl md:m-10"> Votre adresse Email a été vérifiée</p>
+            <p class=" text-center text-2xl md:m-8"> Vous pouvez désormais vous connecter</p>
+        </div>
+
+        <div class="flex items-center justify-center m-25">
+            <img alt="Logo Insalan XVIII" class="image-center" src="/src/assets/images/logo_home.png"/>
+        </div>
+    </div>
+
+</template>
