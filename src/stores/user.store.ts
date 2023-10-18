567c1f99e3b0c9ecafa6c6dbaa5f0206fa09726b
KwikKill <gabriel@blaisot.org>
2023-10-18 22:02:47 +0200

    Refactor user store and Me view components

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -16,16 +16,22 @@ export const useUserStore = defineStore('user', () => {
 	const inscriptions = ref({})
 	const { setContent }  = ToastStore;
 	const { add_error } = ErrorStore;
-	async function get_csrf() {
-		await axios.get('/user/get-csrf/')
-		const cookies = []
-		document.cookie.split(';').forEach(function(cookie) {
+	
+	/*
+	 * Get a new csrf token from the server
+	 * @return Promise<void> 
+	*/
+	async function get_csrf():Promise<void> {
+		await axios.get('/user/get-csrf/');
+		const cookies:Array<{name: string, value: string}> = []
+		document.cookie.split(';').forEach((cookie) => {
 			cookies.push({
 				name: cookie.split('=')[0],
 				value: cookie.split('=')[1]
-			})
+			});
 		});
-		csrf.value = cookies.find(cookie => cookie.name == "csrftoken")['value']
+		const token = cookies.find(cookie => cookie.name === "csrftoken");
+		csrf.value = token ? token.value : "";
 	}
 
 	async function signin(email: String, username: String, password: String, password_validation: String) {
@@ -76,39 +82,51 @@ export const useUserStore = defineStore('user', () => {
 		location = "/";
 
 	}
-	async function fetch_user_inscription_full(user_id: String) {
+
+	/*
+	 * Fetch all the inscription of a user
+	 * @param user_id: the id of the user
+	 * @return Promise<void> 
+	*/
+	async function fetch_user_inscription_full(user_id: String): Promise<void> {
 		try {
-			let ongoing = ref([])
-			let past = ref([])
+			// ref object to store the data
+			let ongoing = ref([]);
+			let past = ref([]);
+			let unpaid = ref({});
 
-			let unpaid = ref({})
-
-			const InscriptionId = await axios.get(`/tournament/player/fromUserId/${user_id}`)
+			// Get all the inscription of the user
+			const InscriptionId = await axios.get(`/tournament/player/fromUserId/${user_id}`);
 			for(let i = 0; i < InscriptionId.data.length; i++){
-				let inscription_details = await axios.get(`/tournament/player/${InscriptionId.data[i]}`)
-				let teams_details = await axios.get(`/tournament/team/${inscription_details.data.team}`)
-				inscription_details.data.team = teams_details.data
-				let tournament_details = await axios.get(`/tournament/tournament/${teams_details.data.tournament}/full`)
-				inscription_details.data.team.tournament = tournament_details.data
-				console.log(inscription_details.data)
+				// Get the details of the inscription
+				let inscription_details = await axios.get(`/tournament/player/${InscriptionId.data[i]}`);
+				// Get the details of the team
+				let teams_details = await axios.get(`/tournament/team/${inscription_details.data.team}`);
+				inscription_details.data.team = teams_details.data;
+				// Get the details of the tournament
+				let tournament_details = await axios.get(`/tournament/tournament/${teams_details.data.tournament}/full`);
+				inscription_details.data.team.tournament = tournament_details.data;
+				console.log(inscription_details.data);
 		
+				// Add the inscription to the right array
 				if(inscription_details.data.payment_status == "NOTPAID") {
-					unpaid.value[inscription_details.data.team.id] = inscription_details.data
-				}
-		
+					unpaid.value[inscription_details.data.team.id] = inscription_details.data;
+				};
+				// Check if the tournament is ongoing or not
 				if(teams_details.data.tournament.event.ongoing) {
-					ongoing.value.push(inscription_details.data)
+					ongoing.value.push(inscription_details.data);
 				} else {
-					past.value.push(inscription_details.data)
-				}
+					past.value.push(inscription_details.data);
+				};
 			}
+			// Set the value of the ref object
 			inscriptions.value = {
 				ongoing,
 				past,
 				unpaid
-			}
+			};
 		} catch(err) {
-			console.log(err)
+			add_error("Impossible de récupérer vos inscriptions, veuillez réessayer plus tard ou contacter un administrateur");
 		}
 	}
 	async function patch_user(data: Object) {
