c470d116cce93812fc151f7ee73057d6084bb6ed
SirZalen <enzosicard2003@gmail.com>
2023-10-18 20:55:46 +0200

    an other upgrade for player usage

--- src/views/EmailVerif.vue
+++ src/views/EmailVerif.vue
@@ -35,9 +35,9 @@ let response = verifMail(props.idname,props.idtoken)
         <div>
             <p class=" text-center md:text-5xl text-3xl md:m-8 mb-6" > - Création de compte - </p>
             <p class=" text-center md:text-2xl  text-2xl md:m-10 mb-4"> Echec vérification</p>
-            <p class=" text-center md:text-2xl md:m-8"> L'adresse a déjà été vérifié ou le lien est invalide</p>
+            <p class=" text-center md:text-2xl md:m-8"> L'adresse a déjà été vérifiée ou le lien est invalide</p>
             <p class=" text-center md:text-2xl md:m-8 m-4"> Revenir à :</p>
-            <p class="text-center text-2xl md:m-6 m-6" ><router-link class="hover:text-cyan-300 border-white border-4 p-4" to="/register">Mon compte</router-link></p>
+            <p class="text-center text-2xl md:m-6 m-6" ><router-link class="hover:text-cyan-300 border-white border-4 p-4" to="/register">Créer un compte</router-link></p>
         </div>
 
         <div class="flex items-center justify-center md:m-32 m-6">
