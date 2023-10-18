9481eba1343133ad2c1c34bef923789dbc26da01
KwikKill <gabriel@blaisot.org>
2023-10-18 17:23:10 +0200

    Add 404 NotFound view to handle invalid routes

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-row items-center justify-items-center h-64">
+        <div class="flex flex-col items-center justify-items-center w-screen">
+            <h1 class="title">404</h1>
+            <h2 class="text-2xl text-center">Page Invalide</h2>
+            <div>
+                <p class="text-justify m-2">La page que vous cherchez n'existe pas ou a été supprimée. Si vous pensez que ce message est une erreur, n'hésitez pas à nous contacter</p>
+            </div>
+            <router-link to="/" class="m-2"><button class="rounded transition duration-150 ease-in-out p-2 font-bold text-white bg-blue-800 hover:ring hover:ring-pink-500">Page d'accueil</button></router-link>
+        </div>
+    </div>
+</template>
