021b967fe61fadad4d476923e83ab3b88bb7f92b
Dyskal <yoshilexis@gmail.com>
2023-10-19 20:19:35 +0200

    add CI and ESLint config (and change some configs)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-  './index.html',
-  './src/**/*.{vue,ts,js,jsx,tsx}'
+    './index.html',
+    './src/**/*.{vue,ts,js,jsx,tsx}',
   ],
   theme: {
     extend: {
-	    colors: {
-		    'theme-bg': '#2C292D',
-	    },
+      colors: {
+        'theme-bg': '#2C292D',
+      },
     },
   },
-  plugins: [require("@tailwindcss/forms")],
-}
-
+  plugins: [import('@tailwindcss/forms')],
+};
