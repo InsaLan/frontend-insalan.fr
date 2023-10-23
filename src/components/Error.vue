7379b1f24cba908ab71a6432c58db934bb08be38
KwikKill <gabriel@blaisot.org>
2023-10-23 20:04:31 +0200

    Lint some views and components

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
-	//TODO: Refactor to Toast component
-import { useErrorStore } from '../stores/error.store'
-import { storeToRefs } from 'pinia'
-const props = defineProps(['context'])
-const ErrorStore = useErrorStore()
-const {flush_errors } = ErrorStore
-const { errors, has_errors} = storeToRefs(ErrorStore)
+// TODO: Refactor to Toast component
+import { storeToRefs } from 'pinia';
+
+import { useErrorStore } from '../stores/error.store';
+
+const ErrorStore = useErrorStore();
+const { flush_errors } = ErrorStore;
+const { errors, has_errors } = storeToRefs(ErrorStore);
 </script>
 <template>
-	<div v-if="has_errors" class="hover:ring-2 hover:ring-red-950 flex left-1/2 w-min-[500px] fixed bottom-1 -ml-[250px] z-40 items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-red-400 dark:bg-red-800">
-		<div class="ml-3 text-sm font-normal">
-			<ul>
-				<li v-for="error in errors"><em class="font-bold not-italic text-xl">- {{error[0]}}</em><p>{{error[1]}}</p></li>
-			</ul>
-		</div>
-		<button @click="flush_errors" class=" hover:ring-2  hover:bg-red-600 hover:ring-red-600 hover:text-white ml-auto -mx-1.5 -my-1.5  rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 " >
-			<span class="sr-only">Close</span>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 stroke-2">
-			  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" class="w-5 h-5"/>
-			</svg>
-		</button>
-	</div>
+  <div v-if="has_errors" class="w-min-[500px] fixed bottom-1 left-1/2 z-40 mb-4 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow hover:ring-2 hover:ring-red-950 dark:bg-red-800 dark:text-red-400">
+    <div class="ml-3 text-sm font-normal">
+      <ul>
+        <li
+          v-for="error in errors"
+          :key="error[0]"
+        >
+          <em class="text-xl font-bold not-italic">- {{ error[0] }}</em><p>{{ error[1] }}</p>
+        </li>
+      </ul>
+    </div>
+    <button type="button" class=" -m-1.5  ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg p-1.5 hover:bg-red-600 hover:text-white hover:ring-2 hover:ring-red-600 " @click="flush_errors">
+      <span class="sr-only">Close</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 stroke-2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" class="h-5 w-5"/>
+      </svg>
+    </button>
+  </div>
 </template>
--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,204 +1,210 @@
-import { defineStore } from "pinia";
-import { ref, computed } from 'vue';
-import axios from "axios";
-import { useRouter } from "vue-router";
+import axios from 'axios';
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-import { useToastStore } from './toast.store';
 import { useErrorStore } from './error.store';
+import { useToastStore } from './toast.store';
 
 export const useUserStore = defineStore('user', () => {
-	let user = ref({})
-	let isConnected = ref(false)
-	let csrf = ref("")
-	const router = useRouter()
-	const ToastStore = useToastStore()
-	const ErrorStore = useErrorStore()
-	const inscriptions = ref({})
-	const MailVerified = ref (false)
-	const { setContent }  = ToastStore;
-	const { add_error } = ErrorStore;
-	
-	/*
+  const user = ref({});
+  const isConnected = ref(false);
+  const csrf = ref('');
+  const router = useRouter();
+  const ToastStore = useToastStore();
+  const ErrorStore = useErrorStore();
+  const inscriptions = ref({});
+  const MailVerified = ref(false);
+  const { setContent } = ToastStore;
+  const { add_error } = ErrorStore;
+
+  /*
 	 * Get a new csrf token from the server
-	 * @return Promise<void> 
+	 * @return Promise<void>
 	*/
-	async function get_csrf():Promise<void> {
-		await axios.get('/user/get-csrf/');
-		const cookies:Array<{name: string, value: string}> = []
-		document.cookie.split(';').forEach((cookie) => {
-			cookies.push({
-				name: cookie.split('=')[0],
-				value: cookie.split('=')[1]
-			});
-		});
-		const token = cookies.find(cookie => cookie.name === "csrftoken");
-		csrf.value = token ? token.value : "";
-	}
+  async function get_csrf():Promise<void> {
+    await axios.get('/user/get-csrf/');
+    const cookies:{ name: string; value: string }[] = [];
+    document.cookie.split(';').forEach((cookie) => {
+      cookies.push({
+        name: cookie.split('=')[0],
+        value: cookie.split('=')[1],
+      });
+    });
+    const token = cookies.find((cookie) => cookie.name === 'csrftoken');
+    csrf.value = token ? token.value : '';
+  }
 
+  // API Call to verify an email adress (an email with an URL made with the name and the token is sent, we take from it these information and verify them)
+  // If both value are correct, the back set the mail on verified, and return a response(), function continue and MailVerified is set on true
+  // If one (or both) values are incorrect, an error is returned and catch by the classic error catcher, stopping the function : MailVerified stays on false
+  async function verifMail(props): Promise<void> {
+    // I set
+    MailVerified.value = false;
+    await axios.get(`/user/confirm/${props.idname}/${props.idtoken}`);
+    // Active only if the API Call doesn't return an error
+    MailVerified.value = true;
+  }
 
-	// API Call to verify an email adress (an email with an URL made with the name and the token is sent, we take from it these information and verify them)
-	// If both value are correct, the back set the mail on verified, and return a response(), function continue and MailVerified is set on true
-	// If one (or both) values are incorrect, an error is returned and catch by the classic error catcher, stopping the function : MailVerified stays on false
-	async function verifMail(idname: String , idtoken: String){
-		//I set 
-		MailVerified.value=false
-		await axios.get(`/user/confirm/${idname}/${idtoken}`)
-		//Active only if the API Call doesn't return an error
-		MailVerified.value=true
-	}
+  async function signin(email: String, username: String, password: String, password_validation: String) {
+    get_csrf();
+    const data = {
+      username,
+      email,
+      password,
+      password_validation,
+    };
+    console.log(data);
+    const res = await axios.post('/user/register/', data, { headers: { 'Content-Type': 'application/json' } });
 
-	async function signin(email: String, username: String, password: String, password_validation: String) {
-		get_csrf()
-		const data = {
-			username,
-			email,
-			password,
-			password_validation
-		}
-		console.log(data)
-		const res = await axios.post('/user/register/', data, { headers: { 'Content-Type': 'application/json'}}) 
-		
-		setContent(`Un email de confirmation vous a été envoyé a ${email} pour confirmer votre compte` , "success")
-	}
+    setContent(`Un email de confirmation vous a été envoyé a ${email} pour confirmer votre compte`, 'success');
+  }
 
-	async function login(username: String, password: String) {
-		get_csrf()
+  async function login(username: String, password: String) {
+    get_csrf();
 
-		try {
-			const res = await axios.post("/user/login/",
-			{
-				username,
-				password
-			}, {
-				withCredentials: true
-			})
-			
-			const user_data = await axios.get('/user/me/', {withCredentials: true})
-			user.value = user_data.data
-			isConnected.value = true
-			setContent(`Bienvenue ${username}`, "success")
-			router.push("/me")
-		} catch(err) {}
-	}
-	async function ask_reset_password(email: String) {
-		get_csrf()
-		const response = await axios.post('/user/password-reset/ask/', {email})
-		setContent(`Un email de confirmation vous a été envoyé a ${email} pour réinitialiser votre compte`, "success")
-	}
-	async function reset_password(user: String, token: String, password: String, password_confirm: String) {
-		get_csrf()
-		const response = await axios.post(`/user/password-reset/submit/`, {user, token, password, password_confirm})
-		setContent(`Votre mot de passe a été réinitialisé`, "success")
-		router.push("/register")
-	}
-	async function logout() {
-		await axios.post("/user/logout/").then(
-			()=> {		
-			isConnected.value = false
-			user.value = {}
-			})
-		location = "/";
+    try {
+      const res = await axios.post(
+        '/user/login/',
+        {
+          username,
+          password,
+        },
 
-	}
+        {
+          withCredentials: true,
+        },
+      );
 
-	/*
+      const user_data = await axios.get('/user/me/', { withCredentials: true });
+      user.value = user_data.data;
+      isConnected.value = true;
+      setContent(`Bienvenue ${username}`, 'success');
+      router.push('/me');
+    } catch (err) {}
+  }
+  async function ask_reset_password(email: String) {
+    get_csrf();
+    const response = await axios.post('/user/password-reset/ask/', { email });
+    setContent(`Un email de confirmation vous a été envoyé a ${email} pour réinitialiser votre compte`, 'success');
+  }
+  async function reset_password(user: String, token: String, password: String, password_confirm: String) {
+    get_csrf();
+    const response = await axios.post('/user/password-reset/submit/', {
+      user, token, password, password_confirm,
+    });
+    setContent('Votre mot de passe a été réinitialisé', 'success');
+    router.push('/register');
+  }
+  async function logout() {
+    await axios.post('/user/logout/').then(
+      () => {
+        isConnected.value = false;
+        user.value = {};
+      },
+    );
+    location = '/';
+  }
+
+  /*
 	 * Fetch all the inscription of a user
 	 * @param user_id: the id of the user
-	 * @return Promise<void> 
+	 * @return Promise<void>
 	*/
-	async function fetch_user_inscription_full(user_id: String): Promise<void> {
-		try {
-			// ref object to store the data
-			let ongoing = ref([]);
-			let past = ref([]);
-			let unpaid = ref({});
+  async function fetch_user_inscription_full(user_id: String): Promise<void> {
+    try {
+      // ref object to store the data
+      const ongoing = ref([]);
+      const past = ref([]);
+      const unpaid = ref({});
 
-			// Get all the inscription of the user
-			const InscriptionId = await axios.get(`/tournament/player/fromUserId/${user_id}`);
-			for(let i = 0; i < InscriptionId.data.length; i++){
-				// Get the details of the inscription
-				let inscription_details = await axios.get(`/tournament/player/${InscriptionId.data[i]}`);
-				// Get the details of the team
-				let teams_details = await axios.get(`/tournament/team/${inscription_details.data.team}`);
-				inscription_details.data.team = teams_details.data;
-				// Get the details of the tournament
-				let tournament_details = await axios.get(`/tournament/tournament/${teams_details.data.tournament}/full`);
-				inscription_details.data.team.tournament = tournament_details.data;
-				console.log(inscription_details.data);
-		
-				// Add the inscription to the right array
-				if(inscription_details.data.payment_status == "NOTPAID") {
-					unpaid.value[inscription_details.data.team.id] = inscription_details.data;
-				};
-				// Check if the tournament is ongoing or not
-				if(teams_details.data.tournament.event.ongoing) {
-					ongoing.value.push(inscription_details.data);
-				} else {
-					past.value.push(inscription_details.data);
-				};
-			}
-			// Set the value of the ref object
-			inscriptions.value = {
-				ongoing,
-				past,
-				unpaid
-			};
-		} catch(err) {
-			add_error("Impossible de récupérer vos inscriptions, veuillez réessayer plus tard ou contacter un administrateur");
-		}
-	}
-	async function patch_user(data: Object) {
-		await get_csrf()
-		try {
-			const res = await axios.patch('/user/me/', data, {
-				withCredentials: true,
-				headers: {
-					'X-CSRFToken': csrf.value,
-					'Content-Type': 'application/json'
-				}
-			})
-			if(res.status == 200) {
-				if(data['current_password']) {
-					setContent("Vos informations ont été modifiées, vous devez vous reconnecter", "success")
-					logout()
-				} else {
-					for(let key in data) {
-						user.value[key] = data[key]
-					}
-					setContent("Vos informations ont été modifiées", "success")
-				}
-			}
-		} catch(err) {
-			if(err.request.status == 403) {
-				add_error({data: {status: err.request.status, messages: "Le mot de passe actuel est différent de celui que vous avez entré"}})
-			} else if(err.request.status == 400) {
-				const response = JSON.parse(err.request.response)
-				if(response["user"]) {
-					add_error({data: {status: err.request.status, messages: response["user"][0]}})
-				} else {
-					add_error({data: {status: err.request.status, messages: response["password"]}})
-				}
-			}
-		}
-	} 
-	const role = computed(()=> {
-		if (user.value.is_superuser) {return "dev" }
-		else if(user.value.is_staff) { return "staff" }
-		else { return "joueur"}
-	})
+      // Get all the inscription of the user
+      const InscriptionId = await axios.get(`/tournament/player/fromUserId/${user_id}`);
+      for (let i = 0; i < InscriptionId.data.length; i++) {
+        // Get the details of the inscription
+        const inscription_details = await axios.get(`/tournament/player/${InscriptionId.data[i]}`);
+        // Get the details of the team
+        const teams_details = await axios.get(`/tournament/team/${inscription_details.data.team}`);
+        inscription_details.data.team = teams_details.data;
+        // Get the details of the tournament
+        const tournament_details = await axios.get(`/tournament/tournament/${teams_details.data.tournament}/full`);
+        inscription_details.data.team.tournament = tournament_details.data;
+        console.log(inscription_details.data);
 
-	return { user, 
-			signin, 
-			login,
-			logout,
-			fetch_user_inscription_full,
-			patch_user,
-			verifMail,
-			ask_reset_password,
-			reset_password,
-			role,
-			isConnected,
-			inscriptions,
-			MailVerified
-	}
-})
+        // Add the inscription to the right array
+        if (inscription_details.data.payment_status == 'NOTPAID') {
+          unpaid.value[inscription_details.data.team.id] = inscription_details.data;
+        }
+        // Check if the tournament is ongoing or not
+        if (teams_details.data.tournament.event.ongoing) {
+          ongoing.value.push(inscription_details.data);
+        } else {
+          past.value.push(inscription_details.data);
+        }
+      }
+      // Set the value of the ref object
+      inscriptions.value = {
+        ongoing,
+        past,
+        unpaid,
+      };
+    } catch (err) {
+      add_error('Impossible de récupérer vos inscriptions, veuillez réessayer plus tard ou contacter un administrateur');
+    }
+  }
+  async function patch_user(data: Object) {
+    await get_csrf();
+    try {
+      const res = await axios.patch('/user/me/', data, {
+        withCredentials: true,
+        headers: {
+          'X-CSRFToken': csrf.value,
+          'Content-Type': 'application/json',
+        },
+      });
+      if (res.status == 200) {
+        if (data.current_password) {
+          setContent('Vos informations ont été modifiées, vous devez vous reconnecter', 'success');
+          logout();
+        } else {
+          for (const key in data) {
+            user.value[key] = data[key];
+          }
+          setContent('Vos informations ont été modifiées', 'success');
+        }
+      }
+    } catch (err) {
+      if (err.request.status == 403) {
+        add_error({ data: { status: err.request.status, messages: 'Le mot de passe actuel est différent de celui que vous avez entré' } });
+      } else if (err.request.status == 400) {
+        const response = JSON.parse(err.request.response);
+        if (response.user) {
+          add_error({ data: { status: err.request.status, messages: response.user[0] } });
+        } else {
+          add_error({ data: { status: err.request.status, messages: response.password } });
+        }
+      }
+    }
+  }
+  const role = computed(() => {
+    if (user.value.is_superuser) { return 'dev'; }
+    if (user.value.is_staff) { return 'staff'; }
+		 return 'joueur';
+  });
+
+  return {
+    user,
+    signin,
+    login,
+    logout,
+    fetch_user_inscription_full,
+    patch_user,
+    verifMail,
+    ask_reset_password,
+    reset_password,
+    role,
+    isConnected,
+    inscriptions,
+    MailVerified,
+  };
+});
