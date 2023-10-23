78659b0822838bedfa2c0aa988dae86866f5413f
Mahal <12588690+ShiroUsagi-san@users.noreply.github.com>
2023-10-23 19:47:25 +0200

    clearing partner

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,28 +1,31 @@
-<script setup>
-import axios from "axios"
-import { ref } from 'vue'
-let partners = ref({})
-axios.get("/partners/").then((data) => {
-	partners.value = data.data.results
-	console.log(data.data.results)
-})
+<script setup lang='ts'>
+import axios from 'axios';
+import { ref } from 'vue';
 
-const placeholder = "./src/assets/images/placeholder_tournament.webp"
+import type { Partner } from './models/partner';
 
+const partners: Partner[] = ref([]);
+try {
+  const partnerData = await axios.get<Partner[]>('/partners/');
+  partners.value = partnerData.data;
+} catch (e) { /* empty */ }
+const placeholder = './src/assets/images/placeholder_tournament.webp';
 
 </script>
 <template>
-	<div>
-	<section>
-		<div class="title text-white"> Nos partenaires</div>
-		<div class="px-4 grid grid-cols-4 gap-3 mb-12">
-			<div class="container h-min max-w-sm break-words bg-cyan-900" v-for="partner in partners" >
-			<a :href="partner.url">
-				<img :src="placeholder" />
-				<p class="text-center">{{partner.name }}</p>
-			</a>
-			</div>
-		</div>
-	</section>
-	</div>
+  <div>
+    <section>
+      <div class="title text-white">
+        Nos partenaires
+      </div>
+      <div class="mb-12 grid grid-cols-4 gap-3 px-4">
+        <div v-for="partner in partners" :key="partner.id" class="container h-min max-w-sm break-words bg-cyan-900">
+          <a :href="partner.url">
+            <img alt="Partner logo" :src="placeholder"/>
+            <p class="text-center">{{ partner.name }}</p>
+          </a>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
---
+++ src/models/partner.ts
@@ -0,0 +1,12 @@
+enum PartnerType {
+  PARTNER = 'PA',
+  SPONSOR = 'SP',
+}
+
+export interface Partner {
+  id: number;
+  name: string;
+  url: string;
+  partner_type: PartnerType;
+  logo?: string;
+}
