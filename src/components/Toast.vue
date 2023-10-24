6c4390e222e9c05436d3aee5a5a640246fa1395e
Paul <79766427+TheBloodMan49@users.noreply.github.com>
2023-10-24 14:50:05 +0200

    TeamCard and Toast components linted

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { storeToRefs }  from 'pinia'
-import { useToastStore } from '../stores/toast.store'
-const ToastStore = useToastStore()
-const { msg, visible, toastType } = storeToRefs(ToastStore)
-const { dismiss, setContent } = ToastStore
+import { storeToRefs } from 'pinia';
+
+import { useToastStore } from '../stores/toast.store';
+
+const ToastStore = useToastStore();
+const { msg, visible, toastType } = storeToRefs(ToastStore);
+const { dismiss } = ToastStore;
 </script>
 <template>
-	<div v-if="visible" :class="toastType" class="hover:ring-2 flex left-1/2 w-min-[500px] fixed bottom-1 -ml-[250px] z-40 items-center w-full max-w-xs p-4 mb-4 text-white rounded-lg shadow">
-		<div class="ml-3 text-sm font-normal">
-			<p> {{ msg }}</p>
-		</div>
-		<button @click="dismiss" class="hover:ring-2  hover:ring-green-600 hover:bg-green-600 hover:text-white ml-auto -mx-1.5 -my-1.5  rounded-lg p-1.5 inline-flex items-center justify-center h-8 w-8 " >
-			<span class="sr-only">Close</span>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-5 h-5 stroke-2">
-			  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" class="w-5 h-5"/>
-			</svg>
-		</button>
-	</div>
+  <div v-if="visible" :class="toastType" class="w-min-[500px] fixed bottom-1 left-1/2 z-40 mb-4 ml-[250px] flex w-full max-w-xs items-center rounded-lg p-4 text-white shadow hover:ring-2">
+    <div class="ml-3 text-sm font-normal">
+      <p> {{ msg }}</p>
+    </div>
+    <button type="button" class="-m-1.5  ml-auto inline-flex h-8 w-8 items-center justify-center  rounded-lg p-1.5 hover:bg-green-600 hover:text-white hover:ring-2 hover:ring-green-600" @click="dismiss">
+      <span class="sr-only">Close</span>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 stroke-2 text-white">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" class="h-5 w-5"/>
+      </svg>
+    </button>
+  </div>
 </template>
