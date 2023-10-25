5d1eb4117a7ab64f0f8b5680e60b303c706b3b5d
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-10-25 16:48:06 +0200

    partial clean of LoginForm.vue

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,103 +1,111 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from 'vue'
-import { useVuelidate } from '@vuelidate/core'
-import { email, required, sameAs } from '@vuelidate/validators'
-import FormField from '../components/FormField.vue'
-import { useUserStore } from '../stores/user.store'
-import Modal from '../components/Modal.vue';
-const { ask_reset_password, login } = useUserStore()
-import Error from '../components/Error.vue'
-const mustBeTrue = (val) => val == true
+import { useVuelidate } from '@vuelidate/core';
+import { email, required } from '@vuelidate/validators';
+import { computed, reactive, ref } from 'vue';
+
+import { useUserStore } from '../stores/user.store';
+import FormField from './FormField.vue';
+import Modal from './Modal.vue';
+
+const { ask_reset_password, login } = useUserStore();
+
 // Register form validation
 const login_form = reactive({
-	username: '',
-	password: ''
-})
+  username: '',
+  password: '',
+});
 
-const rules = computed(()=>{
-	return {
-	username: { required },
-	password: { required },
-}})
+const rules = computed(() => ({
+  username: { required },
+  password: { required },
+}));
 
-const v$ = useVuelidate(rules, login_form)
+const v$ = useVuelidate(rules, login_form);
 
 const login_user = async () => {
-	const isValid = await v$.value.$validate()
-	console.log(isValid)
-	if (!isValid) return
-	login(login_form.username, login_form.password)
-}
+  const isValid = await v$.value.$validate();
+  console.log(isValid);
+  if (!isValid) return;
+  login(login_form.username, login_form.password);
+};
 
-const modal_open = ref(false)
+const modal_open = ref(false);
 
 const data = reactive({
-	email: '',
-})
-const rules_modal = computed(()=>{
-	return {
-	email: { required, email },
-}})
-const v$_modal = useVuelidate(rules_modal, data)
-
-const validateModal = async () => {
-	const isValid = await v$.value.$validate()
-	if(!isValid) return
-	ask_reset_password(data.email)
-	closeModal()
-}
+  email: '',
+});
+const rules_modal = computed(() => ({
+  email: { required, email },
+}));
+const v$_modal = useVuelidate(rules_modal, data);
 
 const closeModal = () => {
-	modal_open.value = false
-}
+  modal_open.value = false;
+};
+
+const validateModal = async () => {
+  const isValid = await v$.value.$validate();
+  if (!isValid) return;
+  ask_reset_password(data.email);
+  closeModal();
+};
 
 const openModal = () => {
-	modal_open.value = true
-}
+  modal_open.value = true;
+};
 
 </script>
 
 <template>
-  <div class="hover:border-solid hover:border-2 hover:border-sky-500 rounded-lg flex items-center flex-col basis-1/2">
-    <h1 class="text-center text-4xl text-white">Se connecter</h1>
+  <div class="flex basis-1/2 flex-col items-center rounded-lg hover:border-2 hover:border-solid hover:border-sky-500">
+    <h1 class="text-center text-4xl text-white">
+      Se connecter
+    </h1>
     <form class="my-2">
       <FormField v-slot="context" label="Nom d'utilisateur" :validations="v$.username" class="flex flex-col">
-        <input :class="{error: context.invalid}" class="border-2 bg-theme-bg" v-model="login_form.username" type="text" placeholder="John doe" @blur="v$.username.$touch"/>
+        <input v-model="login_form.username" :class="{ error: context.invalid }" class="border-2 bg-theme-bg" type="text" placeholder="John doe" @blur="v$.username.$touch"/>
       </FormField>
       <FormField v-slot="context" label="Mot de passe" :validations="v$.password" class="flex flex-col">
-        <input  :class="{error: context.invalid}" class="border-2 bg-theme-bg" v-model="login_form.password" type="password" placeholder="Mot de passe" @blur="v$.password.$touch"/>
+        <input v-model="login_form.password" :class="{ error: context.invalid }" class="border-2 bg-theme-bg" type="password" placeholder="Mot de passe" @blur="v$.password.$touch"/>
       </FormField>
     </form>
-    <button @click="login_user" class="form-btn">Se connecter</button>
-    <a class="p-1 hover:text-blue-800 hover:cursor-pointer" @click="openModal()">Mot de passe oublié?</a>
+    <button class="form-btn" type="submit" @click="login_user">
+      Se connecter
+    </button>
+    <a class="p-1 hover:cursor-pointer hover:text-blue-800" @click="openModal()">Mot de passe oublié?</a>
   </div>
 
   <Modal v-if="modal_open" @close="closeModal">
-    <template v-slot:icon>
-      <div></div>
+    <template #icon>
+      <div/>
     </template>
-    <template v-slot:title>
-      <h3 class="text-base font-semibold leading-6 text-white-900" id="modal-title">Mot de passe oublié ?</h3>
+    <template #title>
+      <h3 id="modal-title" class="text-white-900 text-base font-semibold leading-6">
+        Mot de passe oublié ?
+      </h3>
     </template>
-    <template v-slot:body>
+    <template #body>
       <form class="mt-2">
-        <FormField v-slot="context" label="Email" :validations="v$_modal.email" class="flex flex-col m-2">
-          <input required :class="{error: context.invalid }" class="border-2 bg-theme-bg" v-model="data.email" type="text" placeholder="John-doe@gmail.com" @blur="v$_modal.email.$touch"/>
+        <FormField v-slot="context" label="Email" :validations="v$_modal.email" class="m-2 flex flex-col">
+          <input v-model="data.email" required :class="{ error: context.invalid }" class="border-2 bg-theme-bg" type="text" placeholder="John-doe@gmail.com" @blur="v$_modal.email.$touch"/>
         </FormField>
       </form>
     </template>
-    <template v-slot:buttons>
+    <template #buttons>
       <button
         type="button"
         class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
         @click="validateModal"
-      >Valider
+      >
+        Valider
       </button>
       <button
         type="button"
         class="inline-flex w-full justify-center rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-300 sm:mt-0 sm:w-auto"
         @click="closeModal"
-      >Anuler</button>
+      >
+        Anuler
+      </button>
     </template>
   </Modal>
 </template>
