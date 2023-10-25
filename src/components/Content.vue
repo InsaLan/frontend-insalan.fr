ec94babcd55079e7d35b515ed8fd90aff71b4d96
Floris Bartra <floris.bartra@gmail.com>
2023-10-25 20:45:47 +0200

    cms

---
+++ src/components/Content.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useContentStore } from '../stores/content.store'
+
+interface Props {
+    name: string
+}
+
+const props = defineProps<Props>()
+
+const contentStore = useContentStore()
+const { fetchStatic, getContent } = contentStore
+
+fetchStatic()
+</script>
+
+<template>
+    <div v-html="getContent(props.name)"></div>
+</template>
+
+<style scoped>
+</style>
