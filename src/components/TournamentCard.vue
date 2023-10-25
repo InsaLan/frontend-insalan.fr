772e964b3e4b511b880bf4b2b98efc99f91acc4c
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-10-25 17:22:42 +0200

    lint TournamentCard.vue

--- src/components/TournamentCard.vue
+++ src/components/TournamentCard.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import { ref } from 'vue';
 
-const tournament = defineProps(["tournament"])
-const tour = tournament.tournament
+const props = defineProps({ tournament: { required: true, type: Object } });
 
-//TODO: make a ts struct fetched from api
-const team = ref(2)
-const maxTeam = ref(32)
-const cashprize = ref(1000000)
-const placeholder = "./src/assets/images/placeholder_tournament.webp"
-const registerUrl = "tournament/"+tour.id+"/register"
+// TODO: make a ts struct fetched from api
+const maxTeam = ref(32);
+const cashprize = ref(1000000);
+const registerUrl = `tournament/${props.tournament.id}/register`;
 </script>
 <template>
   <div class="max-w-1/3 min-w-1/3 container h-min bg-cyan-900 shadow-lg">
-    <img :src="tour.logo" class="w-full overflow-clip"/>
+    <img :src="props.tournament.logo" class="w-full text-clip" :alt="props.tournament.name"/>
     <div>
-      <p class="text-center pt-1">{{tour.teams.length}}/{{maxTeam}} Equipes | Cashprize: {{cashprize}}</p>
+      <p class="pt-1 text-center">
+        {{ props.tournament.teams.length }}/{{ maxTeam }} Equipes | Cashprize: {{ cashprize }}
+      </p>
     </div>
     <div class="flex flex-col place-items-center pb-3">
-      <a class="text-zinc-400 pb-2" href="#">Règlement du tournoi</a>
-      <a :href="registerUrl" class="p-2 bg-green-600 center rounded">S'inscrire</a> <!-- TODO: turns button into component-->
+      <a class="pb-2 text-zinc-400" href="#">Règlement du tournoi</a>
+      <a :href="registerUrl" class="center rounded bg-green-600 p-2">S'inscrire</a> <!-- TODO: turns button into component-->
     </div>
   </div>
 </template>
