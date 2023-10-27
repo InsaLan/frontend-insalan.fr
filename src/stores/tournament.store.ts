1c72040a719bfb7f93d5af78e11e03180f57f5db
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-10-27 15:57:07 +0200

    lint tournament store

--- src/stores/tournament.store.ts
+++ src/stores/tournament.store.ts
@@ -1,82 +1,83 @@
-import { defineStore } from "pinia";
-import { computed, ref } from 'vue';
-import axios from "axios";
+import axios from 'axios';
+import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import type { Tournament } from '@/models/tournament';
 
-const groupBy = (items, key) => items.reduce(
+import type { Event } from '../models/event';
+
+const groupBy = (items: Event[], key: string) => items.reduce<Record<number, Event[]>>(
   (result, item) => ({
     ...result,
-    [item[key]]: [
-      ...(result[item[key]] || []),
+    [item[key] as number]: [
+      ...(result[item[key] as number] || []),
       item,
     ],
-  }), 
+  }),
   {},
 );
 
 export const useTournamentStore = defineStore('tournament', () => {
-	let events = ref({})
-	let event = ref({})
-	let tournaments = ref({})
-	let tournament = ref({})
-	
-	async function fetchEvents() {
-		try {
-			const res = await axios.get('/tournament/event')
-			const ev = res.data
-			const years = groupBy(ev, 'year')
-			events.value = years
-			//console.log(years)
-			} catch(error) {
-			console.log(error)	
-		}
-	}
-	async function fetchThisYear() {
-		const year = new Date().getFullYear()
-		const res = await axios.get(`/tournament/event/year/${year}`)
-		event.value = res.data
-		//console.log(res.data)
+  const events = ref<Record<number, Event[]>>({});
+  const event = ref<Event>();
+  const tournaments = ref<Record<number, Tournament>>({});
+  const tournament = ref<Tournament>();
 
-	}
-	async function fetchTournaments(ids) {
-		tournaments.value = {}
-		for (const id of ids) {
-			try{
-				const res = await axios.get(`/tournament/tournament/${id}`)
-				tournaments.value[id] = res.data
-			} catch(err) {
-				console.log(err)
-			}
-		}
-	}
-	async function fetchTournamentFull(id){
-		try {
-			const res = await axios.get(`/tournament/tournament/${id}/full`)
-			tournament.value = res.data
-			console.log(res.data)
-		} catch(error){
-			console.log(error)
-		}
+  async function fetchEvents() {
+    try {
+      const res = await axios.get<Event[]>('/tournament/event');
+      const ev = res.data;
+      const years = groupBy(ev, 'year');
+      events.value = years;
+      // console.log(years)
+    } catch (error) {
+      // console.log(error);
+    }
+  }
+  async function fetchThisYear() {
+    const year = new Date().getFullYear();
+    const res = await axios.get<Event>(`/tournament/event/year/${year}`);
+    event.value = res.data;
+    // console.log(res.data)
+  }
+  async function fetchTournaments(ids: number[]) {
+    const promises = ids.map<Promise<Tournament | Error>>((id) => axios.get<Tournament>(`/tournament/tournamen/${id}`).then((res) => res.data).catch((err: Error) => err));
 
-	}
-	async function fetchTournament(id){
-		try {
-			const res = await axios.get(`/tournament/tournament/${id}`)
-			tournament.value = res.data
-			console.log(res.data)
-		} catch(error){
-			console.log(error)
-		}
+    const data = await Promise.all<Tournament | Error>(promises);
 
-	}
-	//const archives = computed(() => events.value.filter((event) => event.year <= new Date().getFullYear()))
-	return { events,
-		event,
-		tournaments,
-		tournament,
-		fetchEvents,
-		fetchTournament,
-		fetchTournamentFull,
-		fetchThisYear,
-		fetchTournaments,
-	}
-})
+    data.forEach((e, id) => {
+      if (!(e instanceof Error)) {
+        tournaments.value[ids[id]] = e;
+      }
+    });
+  }
+  async function fetchTournamentFull(id: number) {
+    try {
+      const res = await axios.get<Tournament>(`/tournament/tournament/${id}/full`);
+      tournament.value = res.data;
+      // console.log(res.data);
+    } catch (error) {
+      // console.log(error);
+    }
+  }
+  async function fetchTournament(id: number) {
+    try {
+      const res = await axios.get<Tournament>(`/tournament/tournament/${id}`);
+      tournament.value = res.data;
+      // console.log(res.data);
+    } catch (error) {
+      // console.log(error);
+    }
+  }
+  // const archives = computed(() => events.value.filter((event) => event.year <= new Date().getFullYear()))
+  return {
+    events,
+    event,
+    tournaments,
+    tournament,
+    fetchEvents,
+    fetchTournament,
+    fetchTournamentFull,
+    fetchThisYear,
+    fetchTournaments,
+  };
+});
