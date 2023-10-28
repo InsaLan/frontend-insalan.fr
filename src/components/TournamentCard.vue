db22f8c188d61a5a1c9e2e197e1b4355cf9e0db1
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-10-28 18:08:58 +0200

    fix tournamentCard and Home

--- src/components/TournamentCard.vue
+++ src/components/TournamentCard.vue
@@ -2,25 +2,23 @@
 import { ref } from 'vue';
 import type { Tournament } from '@/models/tournament';
 
-defineProps<{
+const props = defineProps<{
   tournament: Tournament;
 }>();
 
-//TODO: make a ts struct fetched from api
-const team = ref(2)
-const maxTeam = ref(32)
-const cashprize = ref(1000000)
-const placeholder = "./src/assets/images/placeholder_tournament.webp"
-const registerUrl = "tournament/"+tour.id+"/register"
-const detailUrl = "tournament/"+tour.id
+// TODO: make a ts struct fetched from api
+const maxTeam = ref(32);
+const cashprize = ref(1000000);
+const registerUrl = `tournament/${props.tournament.id}/register`;
+const detailUrl = `tournament/${props.tournament.id}`;
 </script>
 <template>
-  <div class="grid bg-cyan-900 shadow-lg place-items-center gap-2">
-    <img :src="tour.logo" class="overflow-clip w-screen aspect-video"/>
-    <p>{{tour.teams.length}}/{{maxTeam}} Equipes | Cashprize: {{cashprize}}</p>
-    <div class="flex w-2/3 justify-between mb-3">
-      <a :href="detailUrl" class="p-2 rounded border-green-600 border-2">Plus d'infos</a>
-      <a :href="registerUrl" class="p-2 bg-green-600 center rounded">S'inscrire</a> <!-- TODO: turns button into component-->
+  <div class="grid place-items-center gap-2 bg-cyan-900 shadow-lg">
+    <img :src="props.tournament.logo" class="aspect-video w-screen text-clip" :alt="`Logo du ${props.tournament.name}`"/>
+    <p>{{ props.tournament.teams.length }}/{{ maxTeam }} Equipes | Cashprize: {{ cashprize }}</p>
+    <div class="mb-3 flex w-2/3 justify-between">
+      <a :href="detailUrl" class="rounded border-2 border-green-600 p-2">Plus d'infos</a>
+      <a :href="registerUrl" class="center rounded bg-green-600 p-2">S'inscrire</a> <!-- TODO: turns button into component-->
     </div>
   </div>
 </template>
