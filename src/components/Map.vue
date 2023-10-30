9ba3e255bfade69467dd4293cbf8677f956fdc00
Mahal <12588690+ShiroUsagi-san@users.noreply.github.com>
2023-10-30 17:05:29 +0100

    adding Map component

---
+++ src/components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1 class="title">
+    Ou nous trouver
+  </h1>
+  <section class="flex justify-center">
+    <div class="h-[512px] w-[512px] border-4">
+      <l-map ref="map" v-model:zoom="zoom" :center="insalan_pos" :use-global-leaflet="false">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+        <l-marker :lat-lng="insalan_pos">
+          <l-tooltip>InsaLan</l-tooltip>
+        </l-marker>
+      </l-map>
+    </div>
+  </section>
+</template>
+
+<script setup lang='ts'>
+import {
+  LMap,
+  LMarker,
+  LTileLayer,
+  LTooltip,
+} from '@vue-leaflet/vue-leaflet';
+import { ref } from 'vue';
+
+import 'leaflet/dist/leaflet.css';
+
+const insalan_pos = [48.12229, -1.63653];
+const zoom = ref(15);
+</script>
--- src/views/Informations.vue
+++ src/views/Informations.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import Map from '@/components/Map.vue';
 </script>
 
 <template>
   <h1>So much informations ici</h1>
+  <Map/>
 </template>
