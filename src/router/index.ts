995022be51047f89d11fdf6c583e7c1f2731158d
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-11-03 01:46:15 +0100

    add default scroll to top router-link navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -75,4 +75,10 @@ const routes: RouteRecordRaw[] = [
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { top: 0 };
+  },
 });
