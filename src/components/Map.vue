9ae0a78a9ca2c264da3cfd12b4f1c0d79ac9aa0e
Dyskal <yoshilexis@gmail.com>
2023-11-10 17:25:01 +0100

    fix map overlapping with mobile menu

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,7 @@
   </h1>
   <section class="flex justify-center">
     <div class="h-[512px] w-[512px] border-4">
-      <l-map ref="map" v-model:zoom="zoom" :center="insalan_pos" :use-global-leaflet="false">
+      <l-map ref="map" v-model:zoom="zoom" :center="insalan_pos" :use-global-leaflet="false" class="z-0">
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
