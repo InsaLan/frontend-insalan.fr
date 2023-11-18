f1be2c0846cc35c7e21d3e856fa65adfab2d1812
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-11-18 22:29:39 +0100

    Add caster and fix game model

--- src/views/TournamentDetail.vue
+++ src/views/TournamentDetail.vue
@@ -5,6 +5,7 @@ import {
 } from 'vue';
 import { useRouter } from 'vue-router';
 import TeamCard from '@/components/TeamCard.vue';
+import type { Game } from '@/models/game';
 import type { Team } from '@/models/team';
 import type { Tournament } from '@/models/tournament';
 import { useContentStore } from '@/stores/content.store';
@@ -168,11 +169,14 @@ onMounted(async () => {
             <polygon points="0,0 8,0 8,4 12,4 12,8 4,8 4,4 0,4"/>
             <rect fill="#d14d7b" x="0" y="3.5" width="4" height="0.5"/>
             <rect fill="#d14d7b" x="4" y="7.5" width="8" height="0.5"/>
-            <text fill="white" font-size="1" text-anchor="middle" x="4" y="1.25">
-              {{ tournament?.maxTeam }} équipes
+            <text v-if="(tournament?.game as Game).players_per_team === 1" fill="white" font-size="1" text-anchor="middle" x="4" y="2.25">
+              {{ tournament?.maxTeam }} joueurs
             </text>
-            <text fill="white" font-size="1" text-anchor="middle" x="4" y="2.75">
-              de X joueurs
+            <text v-else fill="white" font-size="1" text-anchor="middle" x="4" y="1.25">
+              <tspan>{{ tournament?.maxTeam }} équipes</tspan>
+              <tspan text-anchor="middle" x="4" y="2.75">
+                de {{ (tournament?.game as Game).players_per_team }} joueurs
+              </tspan>
             </text>
             <text fill="white" font-size="1" text-anchor="middle" x="8" y="5.25">
               {{ Number(tournament?.player_price_online) }}€ / joueur
@@ -213,15 +217,24 @@ onMounted(async () => {
 
         <div class="relative top-14 flex w-full grow flex-col items-center">
           <h3 class="mb-6 text-4xl">
-            Casteurs
+            Casters
           </h3>
           <svg class="w-1/2" viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg">
             <rect x="0" y="0" width="8" height="8" fill="#B169BF"/>
             <rect x="0" y="7.5" width="8" height="0.5" fill="#844A8F"/>
             // eslint-disable-next-line tailwindcss/migration-from-tailwind-2
-            <image :href="tournament?.logo" class="overflow-clip" width="4" height="3.5"/>
-            <text fill="white" font-size="1" text-anchor="middle" x="6" y="6.25">
-              LGC
+            <!-- <image :href="tournament?.logo" class="overflow-clip" width="4" height="3.5"/> -->
+            <text v-if="tournament?.casters.length === 0" fill="white" font-size="1">
+              <tspan x="4" y="3.5" text-anchor="middle">Aucun caster</tspan>
+              <tspan x="4" y="5" text-anchor="middle">pour ce tournoi</tspan>
+            </text>
+            <text v-else-if="tournament?.casters.length === 1" fill="white" font-size="1" text-anchor="middle" x="4" y="4.25">
+              {{ tournament.casters[0].pseudo }}
+            </text>
+            <text v-else-if="tournament?.casters.length === 2" fill="white" font-size="1">
+              <tspan x="0.5" y="2.25" text-anchor="start">{{ tournament.casters[0].pseudo }}</tspan>
+              <tspan text-anchor="middle" x="4" y="4.25">&</tspan>
+              <tspan text-anchor="end" x="7.5" y="6.25">{{ tournament.casters[1].pseudo }}</tspan>
             </text>
           </svg>
         </div>
