31700e73947b3376e39ed6f5a41e1c5ea3b50ef6
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2023-11-18 22:29:39 +0100

    Add caster and fix game model

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -6,12 +6,12 @@ import { useUserStore } from '@/stores/user.store';
 const { user } = storeToRefs(useUserStore());
 
 defineProps<{
-  team: Team;
+  team: Team | number;
 }>();
 </script>
 
 <template>
-  <div class="overflow-hidden text-ellipsis rounded bg-cyan-500 p-4">
+  <div v-if="typeof team !== 'number'" class="overflow-hidden text-ellipsis rounded bg-cyan-500 p-4">
     <h1 class="text-shadow text-center text-3xl font-black" :class="{ 'after:inline-block after:h-6 after:w-6  after:content-[url(\'/src/assets/images/check_with_bg.svg\')]': user.is_staff && team.validated }">
       {{ team.name }}
     </h1>
