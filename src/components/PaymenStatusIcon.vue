eb878c0e06024013f148f204eb2ccdbf9f1831ac
Floris Bartra <floris.bartra@gmail.com>
2023-12-17 21:36:53 +0100

    Add icons for the payment status of players

---
+++ src/components/PaymenStatusIcon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { PaymentStatus } from '@/models/registration';
+
+defineProps<{
+  player: Record<string, string>;
+}>();
+</script>
+
+<template>
+  <img
+    v-if="player.payment_status === PaymentStatus.PAID"
+    src="/src/assets/images/check_with_bg.svg"
+    alt="Logo validé"
+    class="inline-block h-5 w-5"
+    title="Ce joueur a payé"
+  />
+  <fa-awesome-icon
+    v-if="player.payment_status === PaymentStatus.LATER"
+    class="clock-icon h-5 w-5 rounded-full bg-white text-yellow-400"
+    icon="fa-solid fa-clock"
+    title="Ce joueur payera sur place"
+  />
+</template>
