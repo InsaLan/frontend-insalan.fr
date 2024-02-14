f98b7feb0a6ca66eebe62eaeac8123181e38ca19
Dyskal <yoshilexis@gmail.com>
2024-02-14 19:17:18 +0100

    add qr code validation

--- src/views/ScanQrCode.vue
+++ src/views/ScanQrCode.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
+import axios from 'axios';
 import type { DetectedBarcode } from 'barcode-detector/pure';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { QrcodeStream } from 'vue-qrcode-reader';
+import { type QRData, TicketStatus } from '@/models/tickets';
 import { useErrorStore } from '@/stores/error.store';
 
 const { add_error } = useErrorStore();
 
-const scannedValue = ref('');
-// const devices = ref<MediaDeviceInfo[]>([]);
-// const selected = ref<string | null>(null);
+const qrcodeData = ref<QRData | undefined>(undefined);
+const paused = ref(false);
 const torchActive = ref(false);
 const torchNotSupported = ref<boolean | undefined>(undefined);
 
@@ -25,65 +26,69 @@ function paintOutline([detectedCode]: DetectedBarcode[], ctx: CanvasRenderingCon
   ctx.stroke();
 }
 
-function onDetect([detectedCode]: DetectedBarcode[]) {
-  scannedValue.value = detectedCode.rawValue;
-  // TODO if value == api.insalan.fr -> axios vers ce lien
-  // value = retour d'axios
-  // ajouter un btn valider
+async function onDetect([detectedCode]: DetectedBarcode[]) {
+  paused.value = true;
+  if (detectedCode.rawValue.startsWith(`${window.location.origin}/v1/tickets/get/`)) {
+    const url = detectedCode.rawValue.replace(`${window.location.origin}/v1`, '');
+    const res = await axios.get<QRData>(url);
+    qrcodeData.value = res.data;
+  } else {
+    paused.value = false;
+  }
+}
+
+async function validate() {
+  await axios.get(`/tickets/scan/${qrcodeData.value?.token}`);
+  paused.value = false;
+  qrcodeData.value = undefined;
+}
+
+function cancel() {
+  paused.value = false;
+  qrcodeData.value = undefined;
 }
 
 const onError = (err: Error) => {
-  // TODO a traduire
   if (err.name === 'NotAllowedError') {
-    add_error('You need to grant camera access permission');
+    add_error('Vous devez accorder l\'autorisation d\'accès à la caméra');
   } else if (err.name === 'NotFoundError') {
-    add_error('No camera on this device');
+    add_error('Pas d\'appareil photo sur cet appareil');
   } else if (err.name === 'NotSupportedError' || err.name === 'InsecureContextError') {
-    add_error('Secure context required (HTTPS, localhost)');
+    add_error('Contexte sécurisé requis (HTTPS, localhost)');
   } else if (err.name === 'NotReadableError') {
-    add_error('Is the camera already in use ?');
+    add_error('La caméra est-elle déjà utilisée ?');
   } else if (err.name === 'OverconstrainedError') {
-    add_error('Installed cameras are not suitable');
-  } else if (err.name === 'StreamApiNotSupportedError') {
-    add_error('Stream API is not supported in this browser');
+    add_error('Les caméras installées ne sont pas adaptées');
   } else {
     add_error(err.message);
   }
 };
 
-// onMounted(async () => {
-// // Trigger the camera permission request
-// await navigator.mediaDevices.getUserMedia({ video: true });
-// devices.value = (await navigator.mediaDevices.enumerateDevices()).filter(({ kind }) => kind === 'videoinput');
-//
-// if (devices.value.length > 0) {
-//   // The last device is usually the best rear camera
-//   selected.value = devices.value.at(-1)!.deviceId;
-// }
-// });
+const ticketStatus = computed(() => {
+  switch (qrcodeData.value?.status) {
+    case TicketStatus.CANCELLED:
+      return 'Ticket annulé';
+    case TicketStatus.SCANNED:
+      return 'Ticket déjà scanné';
+    case TicketStatus.VALID:
+      return 'Ticket valide';
+    default:
+      return 'Erreur dans le scan du ticket';
+  }
+});
+
+type TorchCapabilities = MediaTrackConstraints & { torch?: boolean };
 </script>
 
 <template>
   <section class="mx-4 flex flex-col items-center gap-4">
-    <!--    <div class="flex flex-col text-xl">-->
-    <!--      <label for="device">Caméras</label>-->
-    <!--      <select id="device" v-model="selected" class="text-black">-->
-    <!--        <option-->
-    <!--          v-for="device in devices"-->
-    <!--          :key="device.label"-->
-    <!--          :value="device.deviceId"-->
-    <!--        >-->
-    <!--          {{ device.label }}-->
-    <!--        </option>-->
-    <!--      </select>-->
-    <!--    </div>-->
-
     <qrcode-stream
+      :paused="paused"
       :torch="torchActive"
       :track="paintOutline"
       @detect="onDetect"
       @error="onError"
-      @camera-on="(capabilities) => (torchNotSupported = !capabilities.torch)"
+      @camera-on="(capabilities: TorchCapabilities) => (torchNotSupported = !capabilities.torch)"
     >
       <button
         :disabled="torchNotSupported"
@@ -98,8 +103,27 @@ const onError = (err: Error) => {
         />
       </button>
     </qrcode-stream>
-    <p class="break-all">
-      {{ scannedValue }}
-    </p>
+
+    <div v-if="qrcodeData">
+      <p>Joueur : {{ qrcodeData.user }}</p>
+      <p>Statut : {{ ticketStatus }}</p>
+      <div class="mb-6 flex gap-7">
+        <button
+          v-if="qrcodeData.status === TicketStatus.VALID"
+          type="button"
+          class="form-btn"
+          @click="validate"
+        >
+          Valider
+        </button>
+        <button
+          type="button"
+          class="form-btn bg-red-600"
+          @click="cancel"
+        >
+          Annuler
+        </button>
+      </div>
+    </div>
   </section>
 </template>
