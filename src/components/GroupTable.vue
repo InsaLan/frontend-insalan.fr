2580b01d8341e80314ab12f55471d327ebfe5025
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2024-02-16 17:38:00 +0100

    fix group layout

--- src/components/GroupTable.vue
+++ src/components/GroupTable.vue
@@ -10,22 +10,32 @@ const get_validated_team_by_id = (id: number) => props.teams.validated_teams.fin
 
 </script>
 <template>
-    <table :key="group.id" border="1" class="text-3xl text-bold border-collapse border border-slate-500 hover:border-4">
-        <thead>
-            <tr>
-            <th colspan="2" class="text-black bg-slate-400">{{ group.name }}</th>
-            </tr>
-            <tr>
-            <th align="center" class="border-separate border border-slate-500 troncate bg-slate-200 text-black">Equipe</th>
-            <th align="center" class="border-separate border border-slate-500 bg-slate-200 text-black">Score</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="team_id in group.teams" :key="team_id">
-            <td align="center" class="border-separate border border-slate-500">{{
-                get_validated_team_by_id(team_id)?.name }}</td>
-            <td align="center" class="border-separate border border-slate-500">{{group.scores[team_id]}}</td>
-            </tr>
-        </tbody>
-    </table>
+  <table :key="group.id" border="1" class="text-bold text-3xl">
+    <thead>
+      <tr>
+        <th colspan="2" class="bg-slate-400 p-3 text-black">
+          {{ group.name }}
+        </th>
+      </tr>
+      <tr>
+        <th align="center" class="troncate border-separate border border-slate-500 bg-slate-200 p-4 text-black">
+          Equipe
+        </th>
+        <th align="center" class="border-separate border border-slate-500 bg-slate-200 p-4 text-black">
+          Score
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="team_id in group.teams" :key="team_id">
+        <td align="center" class="border-separate border border-slate-500 p-4">
+          {{
+            get_validated_team_by_id(team_id)?.name }}
+        </td>
+        <td align="center" class="border-separate border border-slate-500 p-4">
+          {{ group.scores[team_id] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
