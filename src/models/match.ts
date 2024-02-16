ac6d19643ff81f8b6539e543ae02a63dc3e0ce7d
Mahal <12588690+ShiroUsagi-san@users.noreply.github.com>
2024-02-16 01:22:01 +0100

    WIP enter game score

--- src/models/match.ts
+++ src/models/match.ts
@@ -13,17 +13,38 @@ export enum BestofType {
   BO7 = 7,
   RANKING = 0,
 }
+export enum MatchTypeEnum {
+  GROUP = "group",
+  BRACKET = "bracket",
+  SWISS = "swiss",
+}
 
-export interface Match {
-  teams: Team[];
+export interface BaseMatch {
+  id: number;
   round_number: number;
   score: Record<number, number>;
   index_in_round: number;
   status: MatchStatus;
   bo_type: BestofType;
-  times: number[];
 }
 
+export interface MatchType {
+  id: number,
+  type: MatchTypeEnum
+}
+
+export interface Match extends BaseMatch {
+  teams: Team[];
+  times: number[];
+}
+export interface ScorePatch {
+  score: {[id: number]: number};
+  times: number[];
+}
+export interface OngoingMatch extends BaseMatch {
+  teams: {id: number, name: string}[];
+  match_type:  MatchType
+}
 export interface Score {
   team: Team;
   match: Match;
