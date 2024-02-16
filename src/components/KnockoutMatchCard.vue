47f8d0546ede29f25584f887f3e271642a18b6ec
KwikKill <gabriel@blaisot.org>
2024-02-17 00:18:51 +0100

    Fix last lint warning

--- src/components/KnockoutMatchCard.vue
+++ src/components/KnockoutMatchCard.vue
@@ -8,7 +8,8 @@ defineProps<{
 </script>
 
 <template>
-  <div v-for="idx in teamPerMatch" :key="idx" class="flex justify-between bg-slate-700 p-2 text-xl" :class="{ 'bg-slate-900': status == 'ONGOING' }"> <!-- max-w-lg min-w-[22rem]-->
+  <div v-for="idx in teamPerMatch" :key="idx" class="flex justify-between bg-slate-700 p-2 text-xl" :class="{ 'bg-slate-900': status == 'ONGOING' }">
+    <!-- max-w-lg min-w-[22rem]-->
     <div class="text-grey-500">
       {{ teams[idx - 1]?.name || "TBD" }}
     </div> <div v-if="status == 'COMPLETED'" :class="teams[idx - 1]?.is_winner ? 'text-emerald-300' : 'text-white'" class="text-3xl font-black">
