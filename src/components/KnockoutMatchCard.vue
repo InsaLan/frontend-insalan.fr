68da3a242f352d760714121ef30a995d5b435420
KwikKill <gabriel@blaisot.org>
2024-02-18 18:05:57 +0100

    Fix tournament bracket style

--- src/components/KnockoutMatchCard.vue
+++ src/components/KnockoutMatchCard.vue
@@ -10,7 +10,7 @@ defineProps<{
 <template>
   <div v-for="idx in teamPerMatch" :key="idx" class="flex justify-between bg-slate-700 p-2 text-xl" :class="{ 'bg-slate-900': status == 'ONGOING' }">
     <!-- max-w-lg min-w-[22rem]-->
-    <div class="text-grey-500">
+    <div class="text-grey-500 truncate">
       {{ teams[idx - 1]?.name || "TBD" }}
     </div> <div v-if="status == 'COMPLETED'" :class="teams[idx - 1]?.is_winner ? 'text-emerald-300' : 'text-white'" class="text-3xl font-black">
       {{ teams[idx - 1]?.score }}
