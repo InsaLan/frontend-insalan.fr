5835f0c12ec304977fddb477011507a85f4142fe
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2024-10-26 18:07:51 +0200

    first draft for tournament admin views

---
+++ src/components/TournamentAdminGroupsMatchs.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { TournamentDeref } from '@/models/tournament';
+import { useTournamentStore } from '@/stores/tournament.store';
+
+defineProps<{
+  tournament: TournamentDeref;
+}>();
+
+const tournamentStore = useTournamentStore();
+const { getTournamentTeams } = tournamentStore;
+getTournamentTeams();
+
+</script>
+
+<template>
+  <router-link :to="{ name: 'tournament_admin_groups' }">
+    Gérer les poules
+  </router-link>
+  <button type="button">
+    Lancer les matchs
+  </button>
+
+  <button type="button">
+    Sauvegarder les modifications
+  </button>
+</template>
