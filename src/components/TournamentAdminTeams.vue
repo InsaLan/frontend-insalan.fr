75d7c32ceea1e662252bd5381cc3e35b4a76266d
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2024-10-28 15:50:07 +0100

    improve admin teams seeding form and add validation

--- src/components/TournamentAdminTeams.vue
+++ src/components/TournamentAdminTeams.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { useVuelidate, type ValidationRule } from '@vuelidate/core';
+import { integer, maxValue, minValue } from '@vuelidate/validators';
 import { storeToRefs } from 'pinia';
+import { computed, reactive } from 'vue';
+import FormField from '@/components/FormField.vue';
 import type { TournamentDeref } from '@/models/tournament';
+import { useErrorStore } from '@/stores/error.store';
 import { useTournamentStore } from '@/stores/tournament.store';
 
-defineProps<{
+const { tournament } = defineProps<{
   tournament: TournamentDeref;
 }>();
+
+const ErrorStore = useErrorStore();
+const { add_error } = ErrorStore;
 
 const tournamentStore = useTournamentStore();
 const { getTournamentTeams } = tournamentStore;
@@ -13,8 +21,34 @@ getTournamentTeams();
 
 const { tourney_teams } = storeToRefs(tournamentStore);
 
+const seeding_form = reactive(tourney_teams.value.validated_teams.reduce((res, item) => {
+  res[item.id] = item.seed;
+  return res;
+}, {} as Record<number, number>));
+
+const rules = computed(() => tourney_teams.value.validated_teams.reduce((res, item) => {
+  res[item.id] = {
+    integer,
+    minValueValue: minValue(0),
+    maxValueRef: maxValue(tournament.maxTeam),
+  };
+  return res;
+}, {} as Record<number, ValidationRule>));
+
+const v$ = useVuelidate(rules, seeding_form);
+
 const save_seeds = async () => {
-  console.log("test")
+  const isValid = await v$.value.$validate();
+
+  const non_zero = Object.values(seeding_form).filter((v) => v !== 0);
+
+  const unique = new Set(non_zero);
+
+  if (!isValid) return;
+
+  if (unique.size !== non_zero.length) {
+    add_error('Les seeds doivent être unique');
+  }
 };
 
 </script>
@@ -24,6 +58,13 @@ const save_seeds = async () => {
     Liste des équipes et seeding
   </h4>
   {{ tournament.teams }}
+  {{ seeding_form }}
+  <p class="text-center">
+    Le seed d'une équipe doit être un entier compris entre 1 et {{ tournament.maxTeam }}.
+    Chaque seed doit être unique.
+    Une équipe avec un seed de 0 sera ignorer lors des calculs faisant intervenir le seeding.
+    Toutes les équipes sans seed seront placées aléatoirement après toutes les équipes seeder.
+  </p>
   <table class="mx-auto my-4 border-2 lg:w-3/5">
     <colgroup>
       <col class="w-4/5"/>
@@ -31,8 +72,12 @@ const save_seeds = async () => {
     </colgroup>
     <thead>
       <tr>
-        <th>Équipe</th>
-        <th>Seed</th>
+        <th class="border-2">
+          Équipe
+        </th>
+        <th class="border-2">
+          Seed
+        </th>
       </tr>
     </thead>
     <tbody>
@@ -44,22 +89,24 @@ const save_seeds = async () => {
           {{ team.name }}
         </td>
         <td align="center" class="border">
-          <label for="seed" class="hidden"/>
-          <input id="seed" v-model="team.seed" type="number" class="bg-inherit border-0" min="1" :max="tournament.maxTeam">
+          <FormField
+            v-slot="context"
+            :validations="v$[team.id]"
+          >
+            <label for="seed" class="hidden"/>
+            <input id="seed" v-model="seeding_form[team.id]" type="number" class="border-0 bg-inherit text-center" :class="{ error: context.invalid }">
+          </FormField>
         </td>
       </tr>
     </tbody>
   </table>
-  <button type="submit" class="mx-auto rounded bg-blue-800 p-2 text-xl font-bold text-white transition duration-150 ease-in-out hover:bg-blue-700">
+  <button type="submit" class="mx-auto rounded bg-blue-800 p-2 text-xl font-bold text-white transition duration-150 ease-in-out hover:bg-blue-700" @click="save_seeds">
     Sauvegarder
   </button>
 </template>
 
 <style>
-input::-webkit-inner-spin-button,::-webkit-outer-spin-button {
-  -webkit-appearance: none;
-}
 input[type="number"] {
-  -moz-appearance: textfield;
+  appearance: textfield;
 }
 </style>
