66023165b4e93b1120177e9f18ed7de7cd181583
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2025-01-18 17:27:53 +0100

    create AdminGroupTable

--- src/components/TournamentAdminGroups.vue
+++ src/components/TournamentAdminGroups.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { storeToRefs } from 'pinia';
 import type { TournamentDeref } from '@/models/tournament';
 import { useTournamentStore } from '@/stores/tournament.store';
 
-import GroupTable from './GroupTable.vue';
+import AdminGroupTable from './AdminGroupTable.vue';
 
 defineProps<{
   tournament: TournamentDeref;
@@ -12,8 +11,6 @@ defineProps<{
 const tournamentStore = useTournamentStore();
 const { getTournamentTeams } = tournamentStore;
 getTournamentTeams();
-
-const { tourney_teams } = storeToRefs(tournamentStore);
 </script>
 
 <template>
@@ -32,5 +29,13 @@ const { tourney_teams } = storeToRefs(tournamentStore);
     Générer les matches
   </button>
 
-  <GroupTable :teams="tourney_teams" :group="tournament.groups[0]"/>
+  <div class="mb-10 flex flex-1 flex-wrap justify-center gap-20">
+    <div
+      v-for="(group, idx) in tournament.groups"
+      :key="group.id"
+      class="m-2 lg:m-0 lg:w-2/5"
+    >
+      <AdminGroupTable v-model="(tournament as TournamentDeref).groups[idx]"/>
+    </div>
+  </div>
 </template>
