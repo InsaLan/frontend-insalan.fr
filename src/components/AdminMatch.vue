da7eccdcb033ce7098cff6f5250233ffe5ae18bd
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2025-02-12 00:01:21 +0100

    add score coloration for winner team

--- src/components/AdminMatch.vue
+++ src/components/AdminMatch.vue
@@ -8,7 +8,7 @@ const { match, selected, teamPerMatch } = defineProps<{
   teamPerMatch: number;
 }>();
 
-const { get_validated_team_by_id } = useTournamentStore();
+const { get_validated_team_by_id, is_winning_team } = useTournamentStore();
 
 </script>
 
@@ -67,6 +67,7 @@ const { get_validated_team_by_id } = useTournamentStore();
         </td>
         <td
           class="w-6 text-right"
+          :class="{ 'text-green-500': is_winning_team(match, match.teams[idx - 1]) }"
         >
           {{ match.score[match.teams[idx - 1]] ?? 0 }}
         </td>
