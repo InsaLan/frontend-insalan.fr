f285bbf8b482fded096efecfb30dfdb54c7670eb
Arthur Branchu-Harel <arthur.branchuharel@gmail.com>
2025-02-21 19:49:52 +0100

    rework swiss rounds and brackets public interface

--- src/components/Tournament/KnockoutMatchCard.vue
+++ src/components/Tournament/KnockoutMatchCard.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
+import type { KnockoutMatch } from '@/models/bracket';
+import type { GroupMatch } from '@/models/group';
+import type { SwissMatch } from '@/models/swiss';
+import { useTournamentStore } from '@/stores/tournament.store';
+
+const { is_winning_team, get_validated_team_by_id } = useTournamentStore();
 
 defineProps<{
   teamPerMatch: number;
-  teams: Record<string, string | number | boolean | undefined>[];
-  status: string;
+  match: GroupMatch | KnockoutMatch | SwissMatch;
+  // teams: Record<string, string | number | boolean | undefined>[];
+  // status: string;
 }>();
 </script>
 
 <template>
-  <div v-for="idx in teamPerMatch" :key="idx" class="flex items-center justify-between bg-slate-700 p-2 text-xl" :class="{ 'bg-slate-900': status === 'ONGOING' }">
-    <div class="text-grey-500 truncate">
-      {{ teams[idx - 1]?.name || "TBD" }}
-    </div> <div v-if="status === 'COMPLETED'" :class="teams[idx - 1]?.is_winner ? 'text-emerald-300' : 'text-white'" class="text-2xl font-black">
-      {{ teams[idx - 1]?.score }}
+  <div
+    v-for="idx in teamPerMatch"
+    :key="idx"
+    class="flex items-center justify-between bg-slate-700 p-2 text-xl"
+    :class="{ 'bg-slate-900': match.status === 'ONGOING' }"
+  >
+    <div
+      class="text-grey-500 truncate"
+    >
+      {{ get_validated_team_by_id(match.teams[idx - 1])?.name ?? "TBD" }}
     </div>
-    <div v-else class="text-2xl font-black">
+    <div
+      v-if="match.status === 'COMPLETED'"
+      :class="is_winning_team(match, match.teams[idx - 1]) ? 'text-emerald-300' : 'text-white'"
+      class="text-2xl font-black"
+    >
+      {{ match.score[match.teams[idx - 1]] ?? 0 }}
+    </div>
+    <div
+      v-else
+      class="text-2xl font-black"
+    >
       0
     </div>
   </div>
--- src/components/Tournament/SwissRoundTable.vue
+++ src/components/Tournament/SwissRoundTable.vue
@@ -1,28 +1,74 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import KnockoutMatchCard from '@/components/Tournament/KnockoutMatchCard.vue';
+import type { SwissRound } from '@/models/swiss';
+import { groupBy } from '@/stores/tournament.store';
 
-defineProps<{
-  rounds: Record<string, Record<string, string | Record<string, string | number | boolean | undefined>[]>[]>;
+const { swiss, teamPerMatch } = defineProps<{
+  // rounds: Record<string, Record<string, string | Record<string, string | number | boolean | undefined>[]>[]>;
+  swiss: SwissRound;
   teamPerMatch: number;
-  roundCount: number;
 }>();
+
+const roundCount = computed(() => 2 * swiss.min_score - 1);
 </script>
 
 <template>
-  <div class="grid h-full items-center gap-x-20 gap-y-5" :style="{ 'grid-template-columns': `repeat(${roundCount},26rem)` }">
-    <div v-for="round_idx in roundCount" :key="round_idx" class="flex flex-col">
-      <h1 class="text-4xl">
-        Tour {{ round_idx }}
-      </h1>
+  <div class="grid size-full gap-x-10 gap-y-5" :style="{ 'grid-template-columns': `repeat(${roundCount}, minmax(18rem, 1fr))` }">
+    <h1
+      v-for="round_idx in roundCount"
+      :key="round_idx"
+      class="text-center text-3xl"
+    >
+      Tour {{ round_idx }}
+    </h1>
+    <div
+      v-for="(round_matchs, round_idx) in groupBy(swiss.matchs, 'round_number')"
+      :key="round_idx"
+      class="flex flex-col gap-6"
+    >
+      <div
+        v-for="(matchs, score_group) in groupBy(round_matchs, 'score_group')"
+        :key="score_group"
+        class="border-2 border-gray-500"
+      >
+        <div
+          class="bg-gray-500 text-center"
+        >
+          <div
+            v-if="Number(round_idx) <= swiss.min_score"
+          >
+            {{ Number(round_idx) - 1 - Number(score_group) }} - {{ score_group }}
+          </div>
+          <div v-else>
+            {{ swiss.min_score - 1 - Number(score_group) }} -
+            {{ Number(round_idx) - swiss.min_score + Number(score_group) }}
+          </div>
+        </div>
+
+        <div
+          class="flex flex-col items-center gap-3"
+        >
+          <div
+            v-for="match in matchs"
+            :key="match.id"
+            class="my-3 w-full divide-y px-2"
+          >
+            <KnockoutMatchCard
+              :match="match"
+              :team-per-match="teamPerMatch"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-    <div v-for="round_idx in roundCount" :key="round_idx" class="flex flex-col">
+    <!-- <div v-for="round_idx in roundCount" :key="round_idx" class="flex flex-col">
       <div v-for="(match, idx) in rounds[round_idx]" :key="idx" class="my-3 divide-y">
         <KnockoutMatchCard
           :team-per-match="teamPerMatch"
-          :teams="(match.teams as Record<string, string | number | boolean | undefined>[])"
-          :status="(match.status as string)"
+          :match="match"
         />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
