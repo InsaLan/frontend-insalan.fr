69418e7c8d0b048cd8ee8f73fdfe6c15e3d0b165
pixup1 <pixup1@yahoo.com>
2025-10-22 13:03:04 +0200

    Add Discord to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -55,7 +55,7 @@ const buildYear = BUILD_YEAR;
                 <path
                   d="M 118.47 203.25 H 92.71 c -5.27 -0.05 -9.59 4.17 -9.67 9.44 v 26.51 c 0.06 5.28 4.39 9.5 9.66 9.44 c 0 0 0 0 0 0 h 25.73 c 5.28 0.06 9.6 -4.16 9.67 -9.44 c 0 0 0 0 0 0 v -26.51 C 128.04 207.42 123.73 203.2 118.47 203.25 z M 124.25 239.2 c 0 3.09 -2.5 5.59 -5.58 5.59 c -0.07 0 -0.13 0 -0.2 0 H 92.71 c -3.08 0.11 -5.67 -2.3 -5.78 -5.39 c 0 -0.07 0 -0.13 0 -0.2 v -26.51 c 0 -3.09 2.5 -5.59 5.58 -5.59 c 0.07 0 0.13 0 0.2 0 h 25.73 c 3.08 -0.11 5.67 2.3 5.78 5.39 c 0 0.07 0 0.13 0 0.2 L 124.25 239.2 z"
                   stroke-linecap="round"
-                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_462); fill-rule: nonzero; opacity: 1;"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: url(#SVGID_462); fill-rule: nonzero; opacity: 1;"
                   transform=" translate(-105.57, -225.9451)"
                 />
               </g>
@@ -77,7 +77,7 @@ const buildYear = BUILD_YEAR;
                 <path
                   d="M 105.59 214.14 c -6.51 0 -11.79 5.28 -11.79 11.79 s 5.28 11.79 11.79 11.79 c 6.51 0 11.79 -5.28 11.79 -11.79 S 112.1 214.14 105.59 214.14 L 105.59 214.14 z M 105.59 233.7 c -4.35 0 -7.88 -3.53 -7.88 -7.88 c 0 -4.35 3.53 -7.88 7.88 -7.88 s 7.88 3.53 7.88 7.88 c 0 0.04 0 0.07 0 0.11 c -0.02 4.34 -3.53 7.86 -7.88 7.88 V 233.7 z"
                   stroke-linecap="round"
-                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_472); fill-rule: nonzero; opacity: 1;"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: url(#SVGID_472); fill-rule: nonzero; opacity: 1;"
                   transform=" translate(-105.59, -225.93)"
                 />
               </g>
@@ -100,7 +100,7 @@ const buildYear = BUILD_YEAR;
                   cx="0"
                   cy="0"
                   r="2.77"
-                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: url(#SVGID_482); fill-rule: nonzero; opacity: 1;"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: url(#SVGID_482); fill-rule: nonzero; opacity: 1;"
                 />
               </g>
             </g>
@@ -121,7 +121,7 @@ const buildYear = BUILD_YEAR;
                 <path
                   d="M 224.62 229.37 l 17.83 23.84 l -17.94 19.39 h 4.04 l 15.71 -16.97 l 12.69 16.97 h 13.74 l -18.84 -25.18 l 16.7 -18.04 h -4.04 L 250.06 245 l -11.69 -15.63 H 224.62 z M 230.56 232.34 h 6.31 l 27.88 37.28 h -6.31 L 230.56 232.34 z"
                   stroke-linecap="round"
-                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: rgb(240,240,240); fill-rule: nonzero; opacity: 1;"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(240,240,240); fill-rule: nonzero; opacity: 1;"
                   transform=" translate(-247.6, -250.985)"
                 />
               </g>
@@ -143,8 +143,30 @@ const buildYear = BUILD_YEAR;
                 <path
                   d="M 209.65 211.48 c 1.37 0 2.79 0 4.11 0 h 0.56 v -7.12 c -0.73 0 -1.48 -0.17 -2.26 -0.22 c -1.4 0 -2.79 0 -4.19 0 c -2.12 -0.01 -4.18 0.64 -5.92 1.84 c -1.93 1.42 -3.21 3.55 -3.55 5.92 c -0.16 0.94 -0.25 1.89 -0.28 2.85 c 0 1.59 0 3.18 0 4.78 v 0.81 h -6.79 v 7.93 h 6.73 v 19.92 h 8.38 v -20 h 6.73 l 0.81 -7.85 h -7.82 c 0 0 0 -3.94 0 -5.59 C 206.38 212.23 207.78 211.53 209.65 211.48 z"
                   stroke-linecap="round"
-                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; is-custom-font: none; font-file-url: none; fill: rgb(82,148,183); fill-rule: evenodd; opacity: 1;"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(70,145,252); fill-rule: evenodd; opacity: 1;"
                   transform=" translate(-202.825, -226.1649)"
+                />
+              </g>
+            </g>
+          </svg>
+        </a>
+        <a class="w-14 hover:brightness-75" href="https://discord.com/invite/eWEzRpjmj4" target="_blank" rel="noopener noreferrer">
+          <svg
+            id="discord"
+            viewBox="100 100 50 50"
+            xml:space="preserve"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>
+              Discord
+            </title>
+            <g>
+              <g transform="matrix(2.0 0 0 2.0 100 100)">
+                <path
+                  d="M18.59 5.88997C17.36 5.31997 16.05 4.89997 14.67 4.65997C14.5 4.95997 14.3 5.36997 14.17 5.69997C12.71 5.47997 11.26 5.47997 9.83001 5.69997C9.69001 5.36997 9.49001 4.95997 9.32001 4.65997C7.94001 4.89997 6.63001 5.31997 5.40001 5.88997C2.92001 9.62997 2.25001 13.28 2.58001 16.87C4.23001 18.1 5.82001 18.84 7.39001 19.33C7.78001 18.8 8.12001 18.23 8.42001 17.64C7.85001 17.43 7.31001 17.16 6.80001 16.85C6.94001 16.75 7.07001 16.64 7.20001 16.54C10.33 18 13.72 18 16.81 16.54C16.94 16.65 17.07 16.75 17.21 16.85C16.7 17.16 16.15 17.42 15.59 17.64C15.89 18.23 16.23 18.8 16.62 19.33C18.19 18.84 19.79 18.1 21.43 16.87C21.82 12.7 20.76 9.08997 18.61 5.88997H18.59ZM8.84001 14.67C7.90001 14.67 7.13001 13.8 7.13001 12.73C7.13001 11.66 7.88001 10.79 8.84001 10.79C9.80001 10.79 10.56 11.66 10.55 12.73C10.55 13.79 9.80001 14.67 8.84001 14.67ZM15.15 14.67C14.21 14.67 13.44 13.8 13.44 12.73C13.44 11.66 14.19 10.79 15.15 10.79C16.11 10.79 16.87 11.66 16.86 12.73C16.86 13.79 16.11 14.67 15.15 14.67Z"
+                  stroke-linecap="round"
+                  style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(114,137,218); fill-rule: nonzero; opacity: 1;"
+                  transform=" translate(0.5, 0.5)"
                 />
               </g>
             </g>
@@ -169,19 +191,19 @@ const buildYear = BUILD_YEAR;
         </div>
       </div>
     </div>
-    <div class="col-span-3 m-2 flex flex-row items-center justify-center justify-items-center">
-      <a class="my-1 md:my-3" href="https://aeir.insa-rennes.fr" target="_blank" rel="noopener noreferrer">
+    <div class="col-span-3 my-2 mr-4 flex flex-row items-center justify-center justify-items-center">
+      <a href="https://aeir.insa-rennes.fr" target="_blank" rel="noopener noreferrer">
         <img
           alt="Logo de l'AEIR"
           src="@/assets/images/logo_aeir.png"
-          class="block h-auto max-h-40 max-w-full"
+          class="block h-auto max-h-16 max-w-full"
         />
       </a>
-      <a class="my-1 ml-2 md:my-3" href="https://insa-rennes.fr" target="_blank" rel="noopener noreferrer">
+      <a class="ml-2" href="https://insa-rennes.fr" target="_blank" rel="noopener noreferrer">
         <img
           alt="Logo de l'INSA"
           src="@/assets/images/logo_insa.png"
-          class="block h-auto max-h-40 max-w-full"
+          class="block h-auto max-h-16 max-w-full"
         />
       </a>
     </div>
