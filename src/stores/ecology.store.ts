2f272c011dc179ffae1413e9135fd467b3b04e85
Floris Bartra <floris.bartra@gmail.com>
2025-12-01 14:20:13 +0100

    Add ecology store

---
+++ src/stores/ecology.store.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { defineStore, storeToRefs } from 'pinia';
+import type { TravelData } from '@/models/travelData';
+
+import { useUserStore } from './user.store';
+
+const userStore = useUserStore();
+const { get_csrf } = userStore;
+const { csrf } = storeToRefs(userStore);
+
+export const useEcologyStore = defineStore('ecology', () => {
+  async function sendTravelData(travelData: TravelData): Promise<boolean> {
+    await get_csrf();
+
+    const response = await axios.post('/ecology/travel-data/', travelData, {
+      withCredentials: true,
+      headers: {
+        'X-CSRFToken': csrf.value,
+      },
+    });
+
+    return response.status === 201;
+  }
+
+  return {
+    sendTravelData,
+  };
+});
