27919cf63d84dcd5329a5a6c6b6ffe303dee64ea
Awing <awingding.win@gmail.com>
2026-08-09 13:14:54 +0200

    rename json files to avoid confusion with specific languages variant

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,7 @@
 import { createI18n } from 'vue-i18n';
 
-import en from './lang/en-GB.json';
-import fr from './lang/fr-FR.json';
+import en from './lang/en.json';
+import fr from './lang/fr.json';
 
 const i18n = createI18n({
   legacy: false,
